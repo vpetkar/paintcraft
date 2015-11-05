326df9d3a6483664f098fb8c720dc65b421144da
Ved Petkar <vedpetkar@gmail.com>
2015-11-05 15:24:27 -0500

    populate a ruby hash with white pixels

--- app/controllers/drawer_controller.rb
+++ app/controllers/drawer_controller.rb
@@ -1,4 +1,11 @@
 class DrawerController < ApplicationController
   def draw
+    @pixels = Hash.new("FFFFFF")
+    for i in 0..16
+      for j in 0..16
+        setColor(i, j, "FFFFFF")
+  end
+  def setColor(x, y, code)
+    @pixels[[x,y]] = code
   end
 end
