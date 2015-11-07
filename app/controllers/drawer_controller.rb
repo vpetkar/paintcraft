de16545790c9268d0e73c854b937f33dba6ed793
Ved Petkar <vedpetkar@gmail.com>
2015-11-06 22:42:43 -0500

    reactively store data on server

--- app/controllers/drawer_controller.rb
+++ app/controllers/drawer_controller.rb
@@ -1,24 +1,26 @@
 class DrawerController < ApplicationController
+  @@pixels
   def setColor(location, code)
-    @pixels[location] = code
+    @@pixels[location] = code
   end
   def draw
-    @pixels = Array.new(16) {
+    @@pixels = Array.new(16) {
       Array.new(16) {
         "FFFFFF"
       }
     }
+    @pixels = @@pixels
   end
   def store
-    puts params
     respond_to do |format|
       format.json {
+        @@pixels[params[:x].to_i][params[:y].to_i] = params[:color]
         render json: {color: "TEST"}
       }
     end
   end
   def printPixels
-    @pixels.each {
+    @@pixels.each {
       |k, v|
       puts "#{k} on #{v}"
     }
